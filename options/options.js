3a460228529d4f81792c6345922c61577bb57b46
Tejaswini Pagare <tejaswini.pagare.dms@crowdbotics.com>
2023-05-11 16:43:17 +0530

    [Crowdbotics Platform Commit]
    Crowdbotics Studio: Update app screens

--- options/options.js
+++ options/options.js
@@ -1,12 +1,11 @@
-import { StyleSheet } from "react-native"
-
 export const globalOptions = {
-  name: "sparklinghat1-40539",
-  url: "https://sparklinghat1-40539.botics.co",
-  api: "https://sparklinghat1-40539.botics.co/api/v1"
-}
+  "name": "sparklinghat1-40539",
+  "url": "https://sparklinghat1-40539.botics.co",
+  "api": "https://sparklinghat1-40539.botics.co/api/v1",
+  "initialRoute": "Untitled1"
+};
 export const modulesOptions = {
   "@modules/app-menu": {
     copy: "Routes available!"
   }
-}
+};
